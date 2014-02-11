07ae546dd44837af6965d5dd844a13749b8ac393
mxryan <mxryan@gmail.com>
2014-02-11 11:25:01 -0700

    include test files...

---
+++ index.js
@@ -0,0 +1,21 @@
+/* jshint node: true */
+'use strict';
+
+var path = require('path');
+
+var es = require('event-stream');
+var gutil = require('gulp-util');
+var extend = require('lodash.assign');
+
+var footerPlugin = function(footerText, data) {
+  footerText = footerText || '';
+  return es.map(function(file, cb){
+    file.contents = Buffer.concat([
+      file.contents,
+      new Buffer(gutil.template(footerText, extend({file : file}, data)))
+    ]);
+    cb(null, file);
+  });
+};
+
+module.exports = footerPlugin;
