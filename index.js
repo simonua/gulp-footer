58b44f24b973b1af940a236b9203cd23522073f9
Simon Kurtz <simonkurtz@gmail.com>
2018-01-02 15:57:56 -0500

    Use gulp-util's template.js for backwards compatibility

--- index.js
+++ index.js
@@ -2,15 +2,15 @@
 'use strict';
 
 var es = require('event-stream');
-var gutil = require('gulp-util');
 var extend = require('lodash.assign');
+var template = require('./template');
 
 var footerPlugin = function(footerText, data) {
   footerText = footerText || '';
   return es.map(function(file, cb){
     file.contents = Buffer.concat([
       file.contents,
-      new Buffer(gutil.template(footerText, extend({file : file}, data)))
+      new Buffer(template(footerText, extend({file : file}, data)))
     ]);
     cb(null, file);
   });
